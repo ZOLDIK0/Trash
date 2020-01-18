0647b96d42e49c9ec97f0b7f9fa969970174bd7f
ZOLDIK0 <gpscrambor.4862500@gmail.com>
2020-01-18 07:01:11 +0100

    Update Ali express currency converter _DZ_.user.js

--- Ali express currency converter _DZ_.user.js
+++ Ali express currency converter _DZ_.user.js
@@ -6,7 +6,7 @@
 // @require      https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js
 // ==/UserScript==
 
-let euro_to_dz = 21300;// change this if you need to. if you did you can't change it by ctrl+alt
+let euro_to_dz = 21300;// change this if you need to.
 
 
 let cookie = "sd6sd1v1s5d15a8asd8rey8t4y92vs5d1fdFFF";
@@ -29,7 +29,7 @@ convertPrices();
         if(!price.endsWith('DZ')){
             oldelements.push({element:e,value:price});
             e.innerHTML = e.innerHTML.substr(e.innerHTML.indexOf("-"));
-            e.innerHTML = ""+convert(price.replace(/[\$€\s]/g,"").replace(",","."))+" DZ";
+            e.innerHTML = ""+convert(price.replace(/[\$€\s.]/g,"").replace(",","."))+" DZ";
             newelements.push({element:e,value:e.innerHTML});
         }
     }
